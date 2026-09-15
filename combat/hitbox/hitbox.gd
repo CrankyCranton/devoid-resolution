19226f3e3b06478d0d026109b73ac8e176e75a20
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2026-09-14 20:44:49 -0400

    Fix adrenaline damage bonus bug

--- combat/hitbox/hitbox.gd
+++ combat/hitbox/hitbox.gd
@@ -8,6 +8,7 @@ func take_damage(damage: Damage, instigator: Node) -> void:
 	# TODO: Move to a better place. Maybe it's applied within damage.gd? Although
 	# then I'll need to be careful about linked damage resources sharing instigator.
 	if instigator is Player:
+		damage = damage.duplicate()
 		damage.minimum += instigator.adrenaline
 		damage.maximum += instigator.adrenaline
 	hit.emit(damage, instigator)
